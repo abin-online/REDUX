fd4787d018cf9860f863d6623fa31874c29593a2
Abin Babu <abinbabuonline@gmail.com>
2024-09-23 13:25:20 +0530

    understanding redux

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var toolkit_1 = require("@reduxjs/toolkit");
 var CAKE_ORDERED = 'CAKE_ORDERED';
+var CAKE_RESTOCK = 'CAKE_RESTOCKED';
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        payload: 1
+    };
+}
+function restockCake(qty) {
+    if (qty === void 0) { qty = 1; }
+    return {
+        type: CAKE_RESTOCK,
+        payload: qty
+    };
+}
+var initialState = {
+    numOfCakes: 10,
+    anotherProperty: 0
+};
+var reducer = function (state, action) {
+    if (state === void 0) { state = initialState; }
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return __assign(__assign({}, state), { numOfCakes: state.numOfCakes - 1 });
+        case CAKE_RESTOCK:
+            return __assign(__assign({}, state), { numOfCakes: state.numOfCakes + action.payload });
+        default:
+            return state;
+    }
+};
+var store = (0, toolkit_1.createStore)(reducer);
+console.log('Initial State ', store.getState())
+var unsubscribe = store.subscribe(function () {
+    var state = store.getState();
+    console.log('Updated State ', state)
+});
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch({
+    type: CAKE_RESTOCK,
+    payload: 10
+})
+store.dispatch(restockCake(4));
+unsubscribe();
--- index.ts
+++ index.ts
@@ -1,13 +1,64 @@
-const CAKE_ORDERED : String = 'CAKE_ORDERED'
+import { createStore } from "@reduxjs/toolkit";
 
-function orderCake(): Object{
+const CAKE_ORDERED : string = 'CAKE_ORDERED'
+const CAKE_RESTOCK : string = 'CAKE_RESTOCKED'
+
+interface OrderCakeAction {
+    type: string;
+    payload: number
+}
+
+function orderCake(): OrderCakeAction {
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: 1
+    }
+} //Action creator
+
+function restockCake(qty: number = 1) : OrderCakeAction {
+    return {
+        type: CAKE_RESTOCK,
+        payload: qty
+    }
+} //Action creator
+
+interface CakeState {
+    numOfCakes: number;
+    anotherProperty: number
+}
+
+const initialState : CakeState = {
+    numOfCakes: 10,
+    anotherProperty: 0
+}
+
+const reducer = (state: CakeState = initialState , action: OrderCakeAction): CakeState => {
+    switch(action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+        };
+        case CAKE_RESTOCK: 
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload
+            }
+        default:
+            return state
     }
 }
 
+const store = createStore(reducer);
+const unsubscribe = store.subscribe(():void =>{
+    const state: CakeState = store.getState();
+})
 
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(restockCake(4))
+
+unsubscribe()
 
 
 export{}
